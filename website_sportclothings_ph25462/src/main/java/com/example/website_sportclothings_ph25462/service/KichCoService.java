31f25f544578a70b44eb45fcd90e22ee6cac20ca
Dungtt211203 <dungttph25462@fpt.edu.vn>
2024-02-05 20:38:31 +0700

    CRUD KICH-CO
    CRUD KICH-CO_PH25462

--- website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/service/KichCoService.java
+++ website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/service/KichCoService.java
@@ -1,4 +1,17 @@
 package com.example.website_sportclothings_ph25462.service;
 
+import com.example.website_sportclothings_ph25462.entity.KichCo;
+import com.example.website_sportclothings_ph25462.entity.MauSac;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public interface KichCoService {
+    List<KichCo> getAll();
+
+    void add(KichCo kichCo);
+
+    void remove(UUID id);
 }
