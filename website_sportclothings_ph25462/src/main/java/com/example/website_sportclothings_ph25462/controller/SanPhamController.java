48a2cedf0de2250320ff0b45bd1e8ade18efda87
Dungtt211203 <dungttph25462@fpt.edu.vn>
2024-02-20 17:00:47 +0700

    done_update_sanpham
    done_update_sanpham_PH25462

--- website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/SanPhamController.java
+++ website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/SanPhamController.java
@@ -2,10 +2,13 @@ package com.example.website_sportclothings_ph25462.controller;
 
 import com.example.website_sportclothings_ph25462.entity.SanPham;
 import com.example.website_sportclothings_ph25462.repository.SanPhamRepository;
+import com.example.website_sportclothings_ph25462.service.Impl.SanPhamServiceImpl;
 import com.example.website_sportclothings_ph25462.service.SanPhamService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,12 +25,12 @@ public class SanPhamController {
     @Autowired
     SanPhamService sanPhamService;
 
-    public Map<Integer, String> getDsTrangThai() {
-        Map<Integer, String> dsTrangThai = new HashMap<>();
-        dsTrangThai.put(0, " hoạt động");
-        dsTrangThai.put(1, " không Hoạt động");
-        return dsTrangThai;
-    }
+//    public Map<Integer, String> getDsTrangThai() {
+//        Map<Integer, String> dsTrangThai = new HashMap<>();
+//        dsTrangThai.put(0, " hoạt động");
+//        dsTrangThai.put(1, " không Hoạt động");
+//        return dsTrangThai;
+//    }
 
     @GetMapping("/san-pham/hien-thi")
     public String hienThi(Model model) {
@@ -35,6 +38,22 @@ public class SanPhamController {
         model.addAttribute("sp", new SanPham());
         model.addAttribute("view", "../san_pham/index.jsp");
         return "/san_pham/index";
+    }
+
+    @GetMapping("/san-pham/view-update/{id}")
+    public String update(@PathVariable UUID id,
+                         Model model) {
+        model.addAttribute("sanPham", sanPhamService.update(id));
+        return "/san_pham/view_update";
+    }
+
+    @PostMapping("/san-pham/view-update/{id}")
+    public String update(
+            @PathVariable UUID id, @ModelAttribute("sanPham") SanPham sanPham
+    ) {
+        sanPham.setId(id);
+        sanPhamService.add(sanPham);
+        return "redirect:/san-pham/hien-thi";
     }
 
     @GetMapping("/san-pham/hien-thi-add")
