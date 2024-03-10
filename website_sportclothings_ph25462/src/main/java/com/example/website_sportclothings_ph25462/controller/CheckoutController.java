5ed3b211d4b7b546e482fa369ce595f931d23377
hoanglcph27504 <118108176+hoanglcph27504@users.noreply.github.com>
2024-03-10 11:47:07 +0700

    update dia chi + gio hang

--- website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/CheckoutController.java
+++ website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/CheckoutController.java
@@ -1,19 +1,35 @@
 package com.example.website_sportclothings_ph25462.controller;
 
+import com.example.website_sportclothings_ph25462.entity.GioHangChiTiet;
+import com.example.website_sportclothings_ph25462.repository.DiaChiRepository;
+import com.example.website_sportclothings_ph25462.service.GioHangChiTietService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 @Controller
 public class CheckoutController {
+
+//    @Autowired
+//    private GioHangChiTietService gioHangChiTietService;
+
+    @Autowired
+    private DiaChiRepository diaChiRepository;
 
     @GetMapping("/showCheckout")
     public String showCheckout(Model model) {
 
-//        List<GioHangChiTiet> cart = gioHangChiTietService.getCartItems(); // ??
-
-//        model.addAttribute("cartItems", cartItems);
+        try {
+            model.addAttribute("diaChiList", diaChiRepository.findAll()); // phải hiển thị địa chỉ theo khách hàng dang đăng nhập.
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         return "/checkout/checkout";
     }
+
 }
--- website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/repository/DiaChiRepository.java
+++ website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/repository/DiaChiRepository.java
@@ -1,9 +1,17 @@
 package com.example.website_sportclothings_ph25462.repository;
 
 import com.example.website_sportclothings_ph25462.entity.DiaChi;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-public interface DiaChiRepository extends JpaRepository<DiaChi, UUID> {
+public interface DiaChiRepository extends JpaRepository<DiaChi, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE DiaChi a SET a.trangThai = 0 WHERE a.taiKhoanKH.id = :khachHangId AND a.id != :currentAddressId")
+    void updateAllAddressesToNonPrimary(@Param("khachHangId") Long khachHangId, @Param("currentAddressId") Long currentAddressId);
 }
