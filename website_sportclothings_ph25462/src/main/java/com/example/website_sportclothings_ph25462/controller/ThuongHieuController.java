bcd88c8ba824d86fb49531eba9046b629d92c431
tothu <thuttph28969@fpt.edu.vn>
2024-03-23 00:45:45 +0700

    Phân trang thương hiệu
    Phân trang thương hiệu_ThuThu

--- website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/ThuongHieuController.java
+++ website_sportclothings_ph25462/src/main/java/com/example/website_sportclothings_ph25462/controller/ThuongHieuController.java
@@ -7,6 +7,9 @@ import com.example.website_sportclothings_ph25462.service.ThuongHieuService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,9 +58,12 @@ public class ThuongHieuController {
     }
 
     @GetMapping("/thuong-hieu/hien-thi")
-    public String hienThi(Model model) {
+    public String hienThi(Model model, @RequestParam(defaultValue = "0") int th) {
+        Pageable pageable = PageRequest.of(th, 5);
+        Page<ThuongHieu> page = thuongHieuService.getAll(pageable);
         model.addAttribute("load", thuongHieuService.getAll());
         model.addAttribute("th", new ThuongHieu());
+        model.addAttribute("page", page);
         model.addAttribute("view", "../thuong_hieu/chinh-sach.jsp");
         return "/thuong_hieu/index";
     }
